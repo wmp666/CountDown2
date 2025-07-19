9418b97230a165c68b0ae71978b3204174837d16
无名牌 <2134868121@qq.com>
2025-07-19 14:16:20 +0800

    发布全新倒计时(开发中) 1.新界面(重写) 2.加入关于

--- src/com/wmp/countdown/tools/CDInfo.java
+++ src/com/wmp/countdown/tools/CDInfo.java
@@ -5,7 +5,7 @@ import com.wmp.countdown.tools.uiTools.CDColor;
 public class CDInfo {
 
     public static final String NAME = "Countdown";
-    public static final String VERSION = "3.0.0";
+    public static final String VERSION = "2.0.0";
     public static final String AUTHOR = "WMP";
 
     public static final String DATA_PATH;
